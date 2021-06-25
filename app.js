408e1dbd0839f568d9e44c3a2889bf4990867e65
Coderboyhabib <developer.habibur@gmail.com>
2021-06-25 21:25:08 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let firstName = prompt('What is your name?');
+let gfName = prompt('What is your Gf name?');
+
+let number = Math.random() * 100;
+number = Math.round(number);
+
+alert(firstName + ' ' + 'and' + ' ' + gfName + ' ' + 'love persent is' + ' ' + number + '%')
